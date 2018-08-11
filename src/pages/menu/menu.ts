08a38b9316f367c2ab4a2f1692a6a0a60e894b2c
Mohamed-Allam <Eng_MohamedAllam@hotmail.com>
2018-08-11 22:03:26 +0300

    categories added

--- src/pages/menu/menu.ts
+++ src/pages/menu/menu.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { HomePage } from '../home/home';
+import * as WC from "woocommerce-api"
 
 
 
@@ -13,12 +14,51 @@ export class MenuPage {
 
   // rootPage: any = MenuPage;
   homePage: any;
+  woocommerce : any;
+  categories : any[] = [];
+  temp : any[] = []
   
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+
+    
+
     console.log(" Menu Constructor ")
     
     this.homePage = HomePage;
+
+    var thisObject = this;
+
+    this.woocommerce = WC({
+      url: "http://centstechnology.com/",
+      consumerKey: "ck_a63f4e6cfc8d5a17d6de5a71fd6dfa1f74e6ac69",
+      consumerSecret: "cs_f446377fd510d767d5fdc4a31ae7c9fd8180bafa",
+      wpAPI: true,
+      version: 'wc/v1'
+    });
+    
+    /* this.woocommerce.get('products', function(err, data, res) {
+       console.log(data);
+       console.log(res);
+     });*/
+
+     this.woocommerce.getAsync('products/categories').then(function (data) {
+      
+      thisObject.temp = JSON.parse(data.body);
+
+      for(let i = 0 ; i < thisObject.temp.length ; i++){
+        if(thisObject.temp[i].parent == 0)
+        {
+          if( ! thisObject.categories.find((c) =>{ return c.id == thisObject.temp[i].id }))
+                thisObject.categories.push(thisObject.temp[i])
+        
+        
+      }
+    }
+      
+    }, function (error) {
+      console.log(error);
+    });
   }
 
   ionViewDidLoad() {
